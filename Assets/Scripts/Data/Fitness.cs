263836f856926b0777a514687a485729bc1b0813
Sean Walton <sean@pillbug.zone>
2019-10-24 15:51:39 +0100

    Added enemy and treasure coverage measures

--- Assets/Scripts/Data/Fitness.cs
+++ Assets/Scripts/Data/Fitness.cs
@@ -6,25 +6,52 @@ public class Fitness
     public DungeonGenome Genome;
 
     public int NumberPassableTiles = 0;
+    public int NumberEnemyTiles = 0;
+    public int NumberTreasureTiles = 0;
 
+    public float EnemyCoverage = 0.0f;
+    public float TreasureCoverage = 0.0f;
 
     public void CalculateFitnesses(DungeonGenome genome)
     {
         Genome = genome;
-        CalculateNumberOfPassableTiles();
+        CalculateNumberOfTiles();
         Debug.Log("Number of Passable Tiles : " + NumberPassableTiles);
+        Debug.Log("Number of Treasure Tiles : " + NumberTreasureTiles);
+        Debug.Log("Number of Enemy Tiles : " + NumberEnemyTiles);
+
+        CalculateEnemyCoverage();
+        CalculateTreasureCoverage();
+        Debug.Log("Enemy Coverage : " + EnemyCoverage);
+        Debug.Log("Treasure Coverage : " + TreasureCoverage);
+
     }
 
-    private void CalculateNumberOfPassableTiles()
+    private void CalculateNumberOfTiles()
     {
         NumberPassableTiles = 0;
+        NumberEnemyTiles = 0;
+        NumberTreasureTiles = 0;
+
         for (int i = 0; i < DungeonGenome.Size; i++)
         {
             for (int j = 0; j < DungeonGenome.Size; j++)
             {
                 if (Genome.DungeonMap[i, j] != DungeonTileType.WALL) NumberPassableTiles++;
+                if (Genome.DungeonMap[i, j] == DungeonTileType.ENEMY) NumberEnemyTiles++;
+                if (Genome.DungeonMap[i, j] == DungeonTileType.TREASURE) NumberTreasureTiles++;
             }
         }
+    }
+
+    private void CalculateEnemyCoverage()
+    {
+        EnemyCoverage = NumberEnemyTiles / (float) NumberPassableTiles;
+    }
+
+    private void CalculateTreasureCoverage()
+    {
+        TreasureCoverage = NumberTreasureTiles / (float) NumberPassableTiles;
     }
 
 }
