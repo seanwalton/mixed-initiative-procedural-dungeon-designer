8af8539101beb90e0f2525b6520ccc9da24b8170
Sean Walton <seanwalton@users.noreply.github.com>
2019-11-05 14:15:55 +0000

    Implemented corridor length detector

---
+++ Assets/Scripts/Data/Corridor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Corridor 
+{
+    public List<Vector2Int> Tiles = new List<Vector2Int>();
+
+    public int Length => Tiles.Count;
+
+    public void Add(Vector2Int node)
+    {
+        Tiles.Add(node);
+    }
+
+}
--- Assets/Scripts/Data/Fitness.cs
+++ Assets/Scripts/Data/Fitness.cs
@@ -22,6 +22,11 @@ public class Fitness
     private List<Node> path;
     private Vector2Int target;
 
+    private bool[,] reachable = new bool[DungeonGenome.Size, DungeonGenome.Size];
+
+    private List<Corridor> corridors = new List<Corridor>();
+    public int[,] CorridorFlag = new int[DungeonGenome.Size, DungeonGenome.Size];
+
     public void CalculateFitnesses(DungeonGenome genome)
     {
         Genome = genome;
@@ -29,7 +34,7 @@ public class Fitness
         CalculateEnemyCoverage();
         CalculateTreasureCoverage();
         CalculateFractalDimension();
-
+        FindCorridors();
         FitnessValue = FractalDimensionFitness * genome.PathFromEntranceToExit.Count;
        
 
@@ -45,13 +50,18 @@ public class Fitness
         {
             for (int j = 0; j < DungeonGenome.Size; j++)
             {
+                reachable[i, j] = false;
                 if (Genome.DungeonMap[i, j] != DungeonTileType.WALL)
                 {
                     NumberPassableTiles++;
                     target.x = i;
                     target.y = j;
                     validPath = PathFinder.FindPath(Genome.EntranceLocation, target, Genome, out path);
-                    if (validPath) NumberReachableTiles++;
+                    if (validPath)
+                    {
+                        NumberReachableTiles++;
+                        reachable[i, j] = true;
+                    }
                 }
                 if (Genome.DungeonMap[i, j] == DungeonTileType.ENEMY) NumberEnemyTiles++;
                 if (Genome.DungeonMap[i, j] == DungeonTileType.TREASURE) NumberTreasureTiles++;
@@ -75,5 +85,97 @@ public class Fitness
         FractalDimensionFitness = Mathf.Max(0, 1.0f - Mathf.Abs(1.35f - FractalDimension));
     }
     
+    private void FindCorridors()
+    {
+        for (int i = 0; i < DungeonGenome.Size; i++)
+        {
+            for (int j = 0; j < DungeonGenome.Size; j++)
+            {
+                CorridorFlag[i, j] = -1;
+            }
+        }
+
+        //Start searching through the dungeon
+        for (int i = 0; i < DungeonGenome.Size; i++)
+        {
+            for (int j = 0; j < DungeonGenome.Size; j++)
+            {
+                if ((CorridorFlag[i,j] < 0) && (reachable[i,j]))
+                {
+                    //This isn't already assigned to a corridor and it's reachable
+                    //Since the loop is forward facing we only need to look forward in i and j
+
+                    //First check in i direction
+                    if (AreAboveAndBelowImpassable(i,j))
+                    {
+                        Corridor corridor = new Corridor();
+                        corridor.Add(new Vector2Int(i, j));
+                        CorridorFlag[i, j] = corridors.Count;
+                        int ii = i;
+                        bool endCorridor = false;
+
+                        while (!endCorridor)
+                        {
+                            ii++;
+                            if ((ii < DungeonGenome.Size) && reachable[ii, j] &&
+                                (AreAboveAndBelowImpassable(ii, j)))
+                            {
+                                corridor.Add(new Vector2Int(ii, j));
+                                CorridorFlag[ii, j] = corridors.Count;
+                            }
+                            else
+                            {
+                                endCorridor = true;
+                            }
+                        }
+
+                        corridors.Add(corridor);
+                    }
+
+                    if (AreLeftAndRightImpassable(i, j))
+                    {
+                        Corridor corridor = new Corridor();
+                        corridor.Add(new Vector2Int(i, j));
+                        CorridorFlag[i, j] = corridors.Count;
+                        int jj = j;
+                        bool endCorridor = false;
+
+                        while (!endCorridor)
+                        {
+                            jj++;
+                            if ((jj < DungeonGenome.Size) && reachable[i,jj] &&
+                                (AreLeftAndRightImpassable(i, jj)))
+                            {
+                                corridor.Add(new Vector2Int(i, jj));
+                                CorridorFlag[i, jj] = corridors.Count;
+                            }
+                            else
+                            {
+                                endCorridor = true;
+                            }
+                        }
+
+                        corridors.Add(corridor);
+                    }
+
+                }
+            }
+        }
+    }
+
+    private bool AreAboveAndBelowImpassable(int i, int j)
+    {
+        //Debug.Log(i.ToString() + " " + j.ToString());
+        return (((j == 0) || (!reachable[i, j - 1]))
+                        && ((j == DungeonGenome.Size - 1) || (!reachable[i, j + 1])));
+    }
+
+    private bool AreLeftAndRightImpassable(int i, int j)
+    {
+        //Debug.Log(i.ToString() + " " + j.ToString());
+        return (((i == 0) || (!reachable[i-1,j]))
+                        && ((i == DungeonGenome.Size - 1) || (!reachable[i+1,j])));
+    }
+
 
 }
