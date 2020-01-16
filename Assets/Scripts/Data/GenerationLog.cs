8ac6dec9b1973f4386d601e1b210b598c7e0283a
Sean Walton <sean@pillbug.zone>
2020-01-16 14:40:50 +0000

    Best individual fitness values now stored in JSON

--- Assets/Scripts/Data/GenerationLog.cs
+++ Assets/Scripts/Data/GenerationLog.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System;
+using System.Collections.Generic;
 
 [Serializable]
 public class GenerationLog 
@@ -7,11 +8,13 @@ public class GenerationLog
     public int GenerationNumber;
     public float BestFitness;
     public float AverageFitness;
+    public List<float> BestFitnessValues;
 
     public GenerationLog(Generation generation, int generationNumber)
     {
         GenerationNumber = generationNumber;
         BestFitness = generation.GetBestFitness();
         AverageFitness = generation.GetAverageFitness();
+        BestFitnessValues = generation.GetBestIndividual().MyFitness.FitnessValues;
     }
 }
